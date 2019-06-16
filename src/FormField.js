6447828eefcf389304fe924f537ffc603ec61312
Samuel Ogden <sam.george.ogden@gmail.com>
2019-06-16 01:00:23 +0100

    feat(FormField): add input options as props

--- src/FormField.js
+++ src/FormField.js
@@ -2,12 +2,16 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 
 import React, { Component } from 'react'
-import { func, string, instanceOf, shape } from 'prop-types'
+import { func, string, instanceOf, shape, bool } from 'prop-types'
+import style from './FormField.css'
 
 export default class FormField extends Component {
   static propTypes = {
     name: string.isRequired,
+    type: string,
+    required: bool,
     title: string.isRequired,
+    description: string,
     validate: func,
     onChange: func,
     next: func,
@@ -19,30 +23,47 @@ export default class FormField extends Component {
     validate: () => true,
     next: () => true,
     refProp: null,
+    type: 'text',
+    required: false,
+    description: '',
   }
 
-  state = { err: '' }
+  state = { value: null, err: '' }
 
-  inputChange = ( { target: { name, value } } ) => {
-    const { validate, onChange } = this.props
-    const res = validate()
-    return res ? onChange( name, value ) : this.setState( { err: res.err } )
+  inputChange = ( { target: { value } } ) => this.setState( { value } )
+
+  next = () => {
+    const { next, validate, onChange, name } = this.props
+    const { value } = this.state
+    const res = validate( value )
+    if ( res ) {
+      onChange( name, value )
+      next()
+    } else {
+      this.setState( { err: res.err } )
+    }
   }
 
   render() {
-    const { name, title, next, refProp } = this.props
+    const { name, title, refProp, type, required, description } = this.props
     const { err } = this.state
     return (
       <div>
-        <h4>{title}</h4>
+        <h4 className={style.title}>{title}</h4>
+        {description !== '' && <p className={style.description}>{description}</p>}
         <input
+          className={style.input}
+          placeholder="Type your answer here"
+          type={type}
           name={name}
           onChange={this.inputChange}
-          onKeyPress={( { key } ) => ( key === 'Enter' ? next() : null )}
+          onKeyPress={( { key } ) => ( key === 'Enter' ? this.next() : null )}
           ref={refProp}
+          required={required}
         />
         <p>{err}</p>
-        <button type="button" onClick={next}>Next</button>
+        <button className={style.nextBtn} type="button" onClick={this.next}>Next</button>
+        <span>Press <span className={style.bold}>Enter</span></span>
       </div>
     )
   }
