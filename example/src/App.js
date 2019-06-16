da657a7d85f5bb60b98ccf3d152f0a3cbdf3c374
Samuel Ogden <sam.george.ogden@gmail.com>
2019-06-16 11:11:00 +0100

    refactor: update app to use child props

--- example/src/App.js
+++ example/src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
 
   render () {
     return (
-        <FormContainer fields={fields} onSubmit={this.onSubmit} >
+        <FormContainer onSubmit={this.onSubmit} >
             <FormField name="fname" validate={() => 1} title="What is your First Name?" required/>
             <FormField name="lname" validate={() => 1} title="And your Last Name?" />
             <FormField name="mname" validate={() => 1} title="What is your Middle Name?" />
@@ -27,19 +27,3 @@ export default class App extends Component {
     )
   }
 }
-
-const fields = [
-  <FormField name="fname" validate={() => 1} title="What is your First Name?" required/>,
-  <FormField name="lname" validate={() => 1} title="And your Last Name?" />,
-  <FormField name="mname" validate={() => 1} title="What is your Middle Name?" />,
-  <FormField name="email" type="email" validate={() => 1} title="What is your Email Address?" />,
-  <FormField name="bday" type="date" validate={() => 1} title="When were you born?" />,
-  <FormField 
-    name="degree" 
-    type="select" 
-    description="For example A levels, bachelors or masters" 
-    validate={() => 1} 
-    title="What is your highest level of education?" 
-  />,
-
-]
