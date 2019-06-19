3fc3b621096195cd388c9650c9cab52cac9712ab
Samuel Ogden <sam.george.ogden@gmail.com>
2019-06-19 16:56:54 +0100

    feat(DateField): update to browser consistent user friendly date field

--- src/DateField.js
+++ src/DateField.js
@@ -2,8 +2,8 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 
 import React from 'react'
-import { string } from 'prop-types'
-import style from './FormField.css'
+import { string, bool } from 'prop-types'
+import style from './DateField.css'
 import FormField from './FormField'
 import Field from './Field'
 
@@ -11,34 +11,85 @@ export default class DateField extends FormField {
   static propTypes = {
     min: string,
     max: string,
+    includeTime: bool,
   }
 
   static defaultProps = {
     type: 'date',
     min: '',
     max: '',
+    includeTime: false,
   }
 
   state = { value: null, err: '' }
 
+  inputChange = ( { target: { name, value } } ) => {
+    const { value: val } = this.state
+    this.setState( { value: ( { ...val, [ name ]: value } ) } )
+  }
+
   render() {
-    const { name, title, refProp, type, required, description, placeholder, min, max } = this.props
+    const { title, refProp, required, description, includeTime } = this.props
     const { err } = this.state
     return (
       <Field title={title} description={description} next={this.next}>
         <div>
-          <input
-            className={style.input}
-            placeholder={placeholder}
-            type={type}
-            name={name}
-            onChange={this.inputChange}
-            onKeyPress={( { key } ) => ( key === 'Enter' ? this.next() : null )}
-            ref={refProp}
-            required={required}
-            min={min}
-            max={max}
-          />
+          <div onKeyPress={( { key } ) => ( key === 'Enter' ? this.next() : null )}>
+            <span className={style.dateInputContainer}>
+              <span className={style.dateTitles}>Day:</span>
+              <input
+                label="day"
+                name="day"
+                type="number"
+                placeholder="dd"
+                onChange={this.inputChange}
+                ref={refProp}
+                required={required}
+                className={style.inputField}
+                min={0}
+                max={31}
+              />
+            </span>
+            <span className={style.dateInputContainer}>
+              <span className={style.dateTitles}>Month:</span>
+              <input
+                name="month"
+                type="number"
+                placeholder="mm"
+                onChange={this.inputChange}
+                required={required}
+                className={style.inputField}
+                min={1}
+                max={12}
+              />
+            </span>
+            <span className={style.dateInputContainer}>
+              <span className={style.dateTitles}>Year:</span>
+              <input
+                name="year"
+                type="number"
+                placeholder="yyyy"
+                onChange={this.inputChange}
+                required={required}
+                className={style.inputField}
+                min={0}
+              />
+            </span>
+            {includeTime
+              && (
+              <span className={style.dateInputContainer}>
+                <span className={style.dateTitles}>Time:</span>
+                <input
+                  name="time"
+                  type="time"
+                  placeholder="00:00"
+                  onChange={this.inputChange}
+                  required={required}
+                  className={style.inputField}
+                />
+              </span>
+              )}
+          </div>
           <p>{err}</p>
         </div>
       </Field>
