d6ac9d2164dbd527046bb1b507a004abae1ace0f
Samuel Ogden <sam.george.ogden@gmail.com>
2019-06-21 15:55:35 +0100

    other: change project name, add documentation to readme

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 
 [![NPM](https://img.shields.io/npm/v/react-form.svg)](https://www.npmjs.com/package/react-form) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
+A react library for building forms with similar functionality to TypeForm.
+
 ## Install
 
 ```bash
-npm install --save react-form
+npm install --save react-formtype
 ```
 
 ## Usage
@@ -15,17 +17,25 @@ npm install --save react-form
 ```jsx
 import React, { Component } from 'react'
 
-import MyComponent from 'react-form'
+import { FormContainer, ShortTextField, NumberField, DateField } from 'react-formtype'
 
 class Example extends Component {
   render () {
     return (
-      <MyComponent />
+      <FormContainer 
+        showProgress={true} 
+        onSubmit={formData => {...}} 
+        submitTitle="Thanks!" 
+        submitButtonText="Send form"
+      >
+        <ShortTextField title="What is your name?" name="name" required />
+        <NumberField title="How many oranges would you like?" name="noranges" min={0} max={10} />
+        <DataField title="When were you born?" name="dob" />
+      </FormContainer>
     )
   }
 }
 ```
-
 ## License
 
 MIT © [Sam-Ogden](https://github.com/Sam-Ogden)
--- example/src/App.js
+++ example/src/App.js
@@ -10,25 +10,17 @@ export default class App extends Component {
 
   render () {
     return (
-        <FormContainer onSubmit={this.submit} showProgress={true}>
-            <ShortTextField 
-              name="fname" 
-              validate={() => 1} 
-              title="What is your First Name?" 
-              maxlength={25} 
-              required />
-            <ShortTextField name="lname" validate={() => 1} title="What is your Last Name?" />
-            <ShortTextField name="email" type="email" validate={() => 1} title="What is your Email Address?" />
-            <DateField name="bday" type="date" validate={() => 1} title="When were you born?" />
-            <NumberField name="ngcses" validate={() => 1} max={10} title="How many GCSEs do you have?" />
-            <ShortTextField 
-              name="degree" 
-              type="select" 
-              description="For example A levels, bachelors or masters" 
-              validate={() => 1} 
-              title="What is your highest level of education?" 
-            />
-        </FormContainer>
+      <FormContainer 
+        showProgress={true} 
+        onSubmit={this.submit}
+        submitTitle="Thanks!" 
+        submitButtonText="Send form"
+        submitDescription="We will be in touch shortly."
+      >
+        <ShortTextField title="What is your name?" name="name" required />
+        <NumberField title="How many oranges would you like?" name="noranges" min={0} max={10} />
+        <DateField title="When were you born?" name="dob" />
+      </FormContainer>
     )
   }
 }
