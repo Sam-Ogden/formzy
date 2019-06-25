b22ae0f31d691ee2bb9468f3a490c243bd072086
Samuel Ogden <sam.george.ogden@gmail.com>
2019-06-25 22:47:45 +0100

    test(MultipleChoiceField): add test for defaultValue

--- src/test/MultipleChoiceField.test.js
+++ src/test/MultipleChoiceField.test.js
@@ -8,30 +8,45 @@ Enzyme.configure( { adapter: new Adapter() } )
 
 const OPTIONS = [ 'A', 'B', 'C' ]
 
+const MCFWithPassingValidation = (
+  <MultipleChoiceField
+    name="field"
+    title="title"
+    options={OPTIONS}
+    onChange={( name, value ) => ( { name, value } )}
+    next={() => 1}
+    validate={() => ''} // validation passes when string is empty
+    refProp={React.createRef( 0 )}
+    defaultValue={[ 'A' ]}
+  />
+)
+
 describe( 'MultipleChoiceField Component', () => {
   it( 'is truthy', () => {
     expect( MultipleChoiceField ).toBeTruthy()
   } )
   test( 'renders', () => {
-    const wrapper = shallow( <MultipleChoiceField name="field" title="field" options={OPTIONS} /> )
+    const wrapper = shallow( MCFWithPassingValidation )
     expect( wrapper.exists() ).toBe( true )
+  } )
+
+  test( 'defaultValue is saved', () => {
+    const hoc = shallow( MCFWithPassingValidation )
+    expect( hoc.instance().state.value ).toEqual( [ 'A' ] )
+  } )
+
+  test( 'inputChange method updates state to value', () => {
+    const newValue = 'newvalue'
+    const hoc = shallow( MCFWithPassingValidation )
+    hoc.instance().inputChange( newValue )
+    expect( hoc.instance().state.value ).toEqual( newValue )
   } )
 
   // Next function is provided by the Higher Order Component in ../components/hocs/withFieldProps
   describe( 'next function with passing validation', () => {
     test( 'onChange prop function is called and returns correct value', () => {
       const name = 'field'
-      const hoc = shallow(
-        <MultipleChoiceField
-          name={name}
-          title="field"
-          options={OPTIONS}
-          onChange={( name, value ) => ( { name, value } )}
-          next={() => 1}
-          validate={() => ''} // validation passes when string is empty
-          refProp={React.createRef( 0 )}
-        />,
-      )
+      const hoc = shallow( MCFWithPassingValidation )
       const child = mount( hoc.get( 0 ) )
       child.instance().onChange( { target: { value: 'A' } } )
       expect( hoc.instance().next() ).toEqual( { name, value: [ 'A' ] } )
@@ -68,14 +83,12 @@ describe( 'MultipleChoiceField Component', () => {
    */
   describe( 'when multiple prop is false', () => {
     test( 'all options are rendered', () => {
-      const wrapper = mount( <MultipleChoiceField name="field" title="field" options={OPTIONS} /> )
+      const wrapper = mount( MCFWithPassingValidation )
       expect( wrapper.find( '.mcf-options-container button' ).length ).toBe( OPTIONS.length )
     } )
 
     test( 'value is received by HOC withFieldProps', () => {
-      const hoc = shallow(
-        <MultipleChoiceField name="field" title="field" options={OPTIONS} refProp={React.createRef( 0 )} />,
-      )
+      const hoc = shallow( MCFWithPassingValidation )
       const child = mount( hoc.get( 0 ) )
       child.instance().onChange( { target: { value: 'A' } } )
       expect( child.state().selected ).toEqual( [ 'A' ] )
@@ -84,9 +97,7 @@ describe( 'MultipleChoiceField Component', () => {
 
     test( 'clicking an option updates state', () => {
       // Access the component of interest from within the HOC wrapper
-      const hoc = shallow(
-        <MultipleChoiceField name="field" title="field" options={OPTIONS} refProp={React.createRef( 0 )} />,
-      )
+      const hoc = shallow( MCFWithPassingValidation )
       const child = mount( hoc.get( 0 ) )
       child.instance().onChange( { target: { value: 'A' } } )
       expect( child.state().selected ).toEqual( [ 'A' ] )
@@ -94,9 +105,7 @@ describe( 'MultipleChoiceField Component', () => {
     } )
 
     test( 'clicking the same option twice doesnt keeps the option in state', () => {
-      const hoc = shallow(
-        <MultipleChoiceField name="field" title="field" options={OPTIONS} refProp={React.createRef( 0 )} />,
-      )
+      const hoc = shallow( MCFWithPassingValidation )
       const child = mount( hoc.get( 0 ) )
       child.instance().onChange( { target: { value: 'A' } } )
       child.instance().onChange( { target: { value: 'A' } } )
@@ -105,9 +114,7 @@ describe( 'MultipleChoiceField Component', () => {
     } )
 
     test( 'clicking 2 options keeps the last option', () => {
-      const hoc = shallow(
-        <MultipleChoiceField name="field" title="field" options={OPTIONS} refProp={React.createRef( 0 )} />,
-      )
+      const hoc = shallow( MCFWithPassingValidation )
       const child = mount( hoc.get( 0 ) )
       child.instance().onChange( { target: { value: 'A' } } )
       child.instance().onChange( { target: { value: 'B' } } )
@@ -124,24 +131,20 @@ describe( 'MultipleChoiceField Component', () => {
    */
   describe( 'when multiple prop is true', () => {
     test( 'all options are rendered', () => {
-      const wrapper = mount( <MultipleChoiceField name="field" title="field" options={OPTIONS} multiple /> )
+      const wrapper = mount( React.cloneElement( MCFWithPassingValidation, { multiple: true } ) )
       expect( wrapper.find( '.mcf-options-container button' ).length ).toBe( OPTIONS.length )
     } )
 
     test( 'clicking an option updates state', () => {
       // Access the component of interest from within the HOC wrapper
-      const hoc = shallow(
-        <MultipleChoiceField name="field" title="field" options={OPTIONS} refProp={React.createRef( 0 )} multiple />,
-      )
+      const hoc = shallow( React.cloneElement( MCFWithPassingValidation, { multiple: true } ) )
       const child = mount( hoc.get( 0 ) )
       child.instance().onChange( { target: { value: 'A' } } )
       expect( child.state().selected ).toEqual( [ 'A' ] )
     } )
 
     test( 'clicking option then again removes it from state', () => {
-      const hoc = shallow(
-        <MultipleChoiceField name="field" title="field" options={OPTIONS} refProp={React.createRef( 0 )} multiple />,
-      )
+      const hoc = shallow( React.cloneElement( MCFWithPassingValidation, { multiple: true } ) )
       const child = mount( hoc.get( 0 ) )
       child.instance().onChange( { target: { value: 'A' } } )
       child.instance().onChange( { target: { value: 'A' } } )
@@ -150,9 +153,7 @@ describe( 'MultipleChoiceField Component', () => {
     } )
 
     test( 'clicking 2 options keeps both', () => {
-      const hoc = shallow(
-        <MultipleChoiceField name="field" title="field" options={OPTIONS} refProp={React.createRef( 0 )} multiple />,
-      )
+      const hoc = shallow( React.cloneElement( MCFWithPassingValidation, { multiple: true } ) )
       const child = mount( hoc.get( 0 ) )
       child.instance().onChange( { target: { value: 'A' } } )
       child.instance().onChange( { target: { value: 'B' } } )
@@ -161,9 +162,7 @@ describe( 'MultipleChoiceField Component', () => {
     } )
 
     test( 'clicking 2 different options and then the first one again, removes the first value', () => {
-      const hoc = shallow(
-        <MultipleChoiceField name="field" title="field" options={OPTIONS} refProp={React.createRef( 0 )} multiple />,
-      )
+      const hoc = shallow( React.cloneElement( MCFWithPassingValidation, { multiple: true } ) )
       const child = mount( hoc.get( 0 ) )
 
       child.instance().onChange( { target: { value: 'A' } } )
