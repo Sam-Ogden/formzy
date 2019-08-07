80ba4784158b0cd0d71140bc46fa2818ec7f180b
Samuel Ogden <sam.george.ogden@gmail.com>
2019-08-07 19:29:37 +0100

    refactor(ProgressBar): removes reactstrap/bootstrap dependency
    - also removes is_js dep
    
    Closes: #1

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { number } from 'prop-types'
-import { Progress } from 'reactstrap'
+import { string, number } from 'prop-types'
 import style from './ProgressBar.css'
 
 /**
@@ -13,14 +12,46 @@ const ProgressBar = ( { progress } ) => (
     <div className={style.container}>
       <div className={style.inner}>
         <p className={style.label}>{`${progress}% completed`}</p>
-        <Progress className={style.bar} color="#66aef7" value={progress} />
+        <Progress progress={progress} />
       </div>
     </div>
   </div>
 )
-
 ProgressBar.propTypes = {
   progress: number.isRequired, // Percent to display
+}
+
+export const Progress = ( { progress, color, height } ) => {
+  const progressStyle = {
+    bar: {
+      height,
+      backgroundColor: '#ccc',
+      borderRadius: '5px',
+    },
+    progress: {
+      height: '100%',
+      backgroundColor: color,
+      WebkitTransition: 'width 1s',
+      transition: 'width 1s',
+      borderRadius: '5px',
+      width: `${progress}%`,
+    },
+  }
+
+  return (
+    <div style={progressStyle.bar}>
+      <div style={progressStyle.progress} />
+    </div>
+  )
+}
+Progress.propTypes = {
+  progress: number.isRequired,
+  color: string,
+  height: string,
+}
+Progress.defaultProps = {
+  color: '#66aef7',
+  height: '1vh',
 }
 
 export default ProgressBar
