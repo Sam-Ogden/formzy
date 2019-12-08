dab2bf0b0a6deb4a0f7ef95865eaf6b6d58a0d28
Samuel Ogden <sam.george.ogden@gmail.com>
2019-12-08 14:14:29 +0000

    refactor: convert form container to functional component

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,4 +1,4 @@
-import React, { Component, Children } from 'react'
+import React, { useState, Children } from 'react'
 import { func, arrayOf, instanceOf, bool, number, oneOfType, shape } from 'prop-types'
 import _ from 'lodash'
 
@@ -17,12 +17,20 @@ const formBackground = { width: '100%',
  * Container component for the form
  * Maintains the form state and scrolling behaviour between form fields
  */
-class FormContainer extends Component {
-  state = { form: {}, submissionErrors: {}, progress: 0 }
-
-  errors = {}
-
-  mapNameToIndex = {}
+function FormContainer( {
+  showProgress,
+  progressStyle,
+  style,
+  scrollDuration,
+  edgeOffset,
+  children,
+  onSubmit,
+} ) {
+  const [ form, setForm ] = useState( {} )
+  const [ submissionErrors, setSubmissionErrors ] = useState( {} )
+  const [ progress, setProgress ] = useState( 0 )
+  let errors = {}
+  const mapNameToIndex = {}
 
   /**
    * Called by Fields to update the form state in the container
@@ -30,105 +38,80 @@ class FormContainer extends Component {
    * @param {Any} newVal The value to update fieldName to
    * @returns {void}
    */
-  onChange = ( fieldName, newVal ) => this.setState(
-    ( { form } ) => ( { form: ( { ...form, [ fieldName ]: newVal } ) } ),
-  )
+  const onChange = ( fieldName, newVal ) => setForm( { ...form, [ fieldName ]: newVal } )
 
   /**
    * Allows fields to register validation errors. This allows checks to occur onSubmit
    * @param {String} fieldName the name of the field in the form with errors
    * @param {Array} err array of strings of errors for given field
    */
-  registerValidationError = ( fieldName, err ) => {
-    const { submissionErrors } = this.state
-
+  const registerValidationError = ( fieldName, err ) => {
     // Clear submission errors for this field since user is trying to rectify them
     if ( submissionErrors[ fieldName ] && submissionErrors[ fieldName ].length !== 0 ) {
-      const newState = { ...submissionErrors, [ fieldName ]: [] }
-      this.setState( { submissionErrors: newState } )
+      setSubmissionErrors( { ...submissionErrors, [ fieldName ]: [] } )
     }
 
     // register new errors without re-render
-    this.errors = { ...this.errors, [ fieldName ]: err }
+    errors = { ...errors, [ fieldName ]: err }
   }
 
   /**
    * Validate inputs and handle result of onSubmit callback
    * @returns {Boolean} true if submission was successful, false otherwise
    */
-  submit = () => {
-    const { onSubmit } = this.props
-    const { form } = this.state
-
+  const submit = () => {
     const isValid = _.reduce(
-      _.keys( this.errors ),
-      ( bool, fieldName ) => ( bool && this.errors[ fieldName ].length === 0 ),
+      _.keys( errors ),
+      ( bool, fieldName ) => ( bool && errors[ fieldName ].length === 0 ),
       true,
     )
 
     if ( isValid ) {
       const result = onSubmit( form )
       if ( result === true || result === {} || result === undefined ) return true
-      this.errors = result
+      errors = result
     }
 
     // Re render with registered errors
-    const firstErrorIndex = this.mapNameToIndex[
-      _.findKey( this.mapNameToIndex, ( i, name ) => (
-        this.errors.name !== undefined
-          ? this.errors[ name ].length > 0
+    const firstErrorIndex = mapNameToIndex[
+      _.findKey( mapNameToIndex, ( i, name ) => (
+        errors.name !== undefined
+          ? errors[ name ].length > 0
           : false
       ) )
     ]
 
-    this.setState( prevState => (
-      { ...prevState, submissionErrors: this.errors }
-    ) )
+    setSubmissionErrors( { ...submissionErrors, submissionErrors: errors } )
 
     return firstErrorIndex
   }
 
-  render() {
-    const {
-      showProgress,
-      progressStyle,
-      style,
-      scrollDuration,
-      edgeOffset,
-      children,
-    } = this.props
-
-    const { form, progress, submissionErrors } = this.state
-
-    return (
-      <form style={{ width: '100%', flexDirection: 'column', display: 'flex' }}>
-        <div style={_.assign( {}, formBackground, style.background )} />
-        <Provider value={form}>
-          <ScrollController
-            scrollDuration={scrollDuration}
-            edgeOffset={edgeOffset}
-            onChange={i => this.setState(
-              { progress: calcProgress( i, Children.count( children ) ) },
-            )}
-            onFinalNext={this.submit}
-          >
-            {Children.map( children,
-              ( Field, i ) => {
-                const { name } = Field.props
-                this.mapNameToIndex[ name ] = i
-                return React.cloneElement( Field, {
-                  onChange: this.onChange,
-                  registerValidationError: this.registerValidationError,
-                  submissionErrors: submissionErrors[ name ],
-                } )
-              } )}
-          </ScrollController>
-        </Provider>
-        {showProgress
+  return (
+    <form style={{ width: '100%', flexDirection: 'column', display: 'flex' }}>
+      <div style={_.assign( {}, formBackground, style.background )} />
+      <Provider value={form}>
+        <ScrollController
+          scrollDuration={scrollDuration}
+          edgeOffset={edgeOffset}
+          onChange={i => setProgress( calcProgress( i, Children.count( children ) ) )}
+          onFinalNext={submit}
+        >
+          {Children.map( children,
+            ( Field, i ) => {
+              const { name } = Field.props
+              mapNameToIndex[ name ] = i
+              return React.cloneElement( Field, {
+                onChange,
+                registerValidationError,
+                submissionErrors: submissionErrors[ name ],
+              } )
+            } )}
+        </ScrollController>
+      </Provider>
+      {showProgress
         && <ProgressBar progress={progress} style={progressStyle} />}
-      </form>
-    )
-  }
+    </form>
+  )
 }
 
 FormContainer.propTypes = {
