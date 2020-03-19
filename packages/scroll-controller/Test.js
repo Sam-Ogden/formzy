4fe082815451d69e3286da847bca58df614e3506
Samuel Ogden <sam.george.ogden@gmail.com>
2020-03-19 19:04:19 +0000

    chore: add typescript, babel, prettier, lerna and eslint configs

---
+++ packages/scroll-controller/Test.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+export default (function() {
+  return React.createElement('div', null);
+});
+//# sourceMappingURL=Test.js.map
---
+++ packages/scroll-controller/src/Test.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const Test = () => <div></div>;
+
+export { Test };
